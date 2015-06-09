bc3997397d5fa127837661dfd77ee3d832c0ead9
John Wu <webmaster@leapoahead.com>
2015-06-10 00:32:24 +0800

    Fix testing problem

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
-var soap = require('soap'),
-  spawn = require('child_process').spawn;
+var soap = require('soap');
 
 require('should');
 
@@ -7,7 +6,8 @@ describe('temperature service', function() {
   it('should be able to get temperature', function(done) {
     var url = 'http://localhost:10001/?wsdl';
     var args = { in: 'placeholder' };
-    soap.createClient(url, function(err, client) {
+    var options = { endpoint: 'http://localhost:10001' };
+    soap.createClient(url, options, function(err, client) {
       client.GetTemperature(function(err, result) {
         result.should.have.property('temperature');
         (+result.temperature).should.be.a.Number;
